efc6ad21eee790dda390e672ec0b88d4e5665248
Your Name <andrew.smiley@live.com>
2017-05-15 17:45:54 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,21 @@ import static java.util.Comparator.comparingDouble;
 import static java.util.Comparator.comparingInt;
 
 /**
- * Created by Andrew on 5/8/17.
+ * Created by Andrew on 5/8/17
+ * Just a simple class to demonstrate dynamic programming.
+ * You are going on a long trip. You start on the road at mile post 0. Along the way there are n
+ hotels, at mile posts a1 < a2 < … < an, where each ai is measured from the starting point. The
+ only places you are allowed to stop are at these hotels, but you can choose which of the hotels
+ you stop at. You must stop at the final hotel (at distance an), which is your destination.
+ You'd ideally like to travel 200 miles a day, but this may not be possible (depending on the spacing
+ of the hotels). If you travel x miles during a day, the penalty for that day is (200 - x)2
+ . You want
+ to plan your trip so as to minimize the total penalty - that is, the sum, over all travel days, of the
+ daily penalties.
+ Give an efficient algorithm that determines the optimal sequence of hotels at which to stop.
+
+ Input files give a list of 100 perspective stops, we need to calculate our the most efficient method for going about the stops.
+
  */
 
 public class Main {
@@ -15,7 +29,7 @@ public class Main {
 
     public static void main(String[] args) {
         //create something to store our mile markers, our penalities and our previous
-        //bests
+        //bests, store 0 as the first element in hte mile markers and penalties, -1 in prvious best to represent 0
         ArrayList<Integer> mileMarkers = new ArrayList<>(Arrays.asList(new Integer(0)));
         ArrayList<Integer> penalities = new ArrayList<>(Arrays.asList(new Integer(0)));
         ArrayList<Integer> previousBest = new ArrayList<>(Arrays.asList(new Integer(-1)));
@@ -59,12 +73,12 @@ public class Main {
         int i = previousBest.size()-1;
         //store the indeexs
         ArrayList<Integer> indexes = new ArrayList<>();
-        //
+        //add our indexes in reverse order (i.e. farthest mile marker first) till we get to -1 i.e. the starting point
         while(i != -1){
             indexes.add(i);
             i = previousBest.get(i);
         }
-
+        //finally, print in decrementing order as to show from mile marker 0 to the last mile marker
         for(int j = indexes.size()-1; j >= 0; j--){
             System.out.println("Distance: "+mileMarkers.get(indexes.get(j))+ " Total Penalty: "+penalities.get(indexes.get(j)));
         }
